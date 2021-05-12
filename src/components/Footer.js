51dbf34df09217755b7570b4c287860600d5f9ec
rafawelsh <mrrafael1912@gmail.com>
2021-05-12 11:23:08 -0700

    edits to DB config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ export default function Footer() {
 			<Section>
 				Designed and Developed by <br />
 				Rafael Castellanos-Welsh <br />
-				@rafaandcoffee
 			</Section>
 		</FooterSection>
 	);
